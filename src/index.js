094e8214dca9429c43785d7faed460ddf5890ec2
romanchenko31 <romanchenko.zhen@gmail.com>
2023-02-17 19:58:13 +0200

    update

--- src/index.js
+++ src/index.js
@@ -3,26 +3,26 @@ module.exports = function check(str, bracketsConfig) {
   return del(str, bracketsConfig);
 
   function del(str, bracketsConfig) {
-    console.log("Оригинальный массив", str);
+    // console.log("Оригинальный массив", str);
     str.map((v, i) => {
-      console.log("Скобка для проверки", v, "Ее Индекс", i);
+      // console.log("Скобка для проверки", v, "Ее Индекс", i);
       bracketsConfig.map((value, index) => {
-        console.log("ARRAY FILTER ", value);
+        // console.log("ARRAY FILTER ", value);
         if (value.includes(v)) {
-          console.log("Фильтр Включает в себя скобку", value);
+          // console.log("Фильтр Включает в себя скобку", value);
           let arr = [];
           arr.push(v);
           arr.push(str[i + 1]);
-          console.log("Наш новый проверочный массив", arr);
-          console.log(
-            "Вторая скобка ФИЛЬТРА",
-            value[index + 1],
-            "Вторая скобка собранного массива",
-            arr[1]
-          );
+          // console.log("Наш новый проверочный массив", arr);
+          // console.log(
+          //   "Вторая скобка ФИЛЬТРА",
+          //   value[1],
+          //   "Вторая скобка собранного массива",
+          //   arr[1]
+          // );
           if (value[1] === arr[1]) {
             str.splice(i, 2);
-            console.log("с Удаленными скобками", str);
+            // console.log("с Удаленными скобками", str);
             del(str, bracketsConfig);
           }
         }
