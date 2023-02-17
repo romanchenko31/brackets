e70a3ec3b79ec3a76502438e76a230626b634799
romanchenko31 <romanchenko.zhen@gmail.com>
2023-02-17 19:49:43 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  str = str.split("");
+  return del(str, bracketsConfig);
+
+  function del(str, bracketsConfig) {
+    console.log("Оригинальный массив", str);
+    str.map((v, i) => {
+      console.log("Скобка для проверки", v, "Ее Индекс", i);
+      bracketsConfig.map((value, index) => {
+        console.log("ARRAY FILTER ", value);
+        if (value.includes(v)) {
+          console.log("Фильтр Включает в себя скобку", value);
+          let arr = [];
+          arr.push(v);
+          arr.push(str[i + 1]);
+          console.log("Наш новый проверочный массив", arr);
+          console.log(
+            "Вторая скобка ФИЛЬТРА",
+            value[index + 1],
+            "Вторая скобка собранного массива",
+            arr[1]
+          );
+          if (value[1] === arr[1]) {
+            str.splice(i, 2);
+            console.log("с Удаленными скобками", str);
+            del(str, bracketsConfig);
+          }
+        }
+      });
+    });
+    return str.length === 0;
+  }
+};
